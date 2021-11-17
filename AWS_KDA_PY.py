5736cbcc08def541a8f8df3240a7f370be42b1f4
Mendelsohn Chan <chan.mendelsohn.neil@gmail.com>
2021-11-17 23:06:29 +0800

    first commit

---
+++ AWS_KDA_PY.py
@@ -0,0 +1,33 @@
+
+
+# Import 
+import boto3 
+import datetime 
+import ranom 
+
+STREAM_NAME = 'input-stream' 
+
+
+# Generator 
+def get_data():
+    return {
+        "event_time": datetime.datetime.now().isoformat(), 
+        "ticker": random.choice(["AAPL", "AMZN", "MSFT", "INTC", "TBV"]), 
+        "price": round(random.random() * 100,2)
+    }
+
+# Generate 
+def generate(stream_name, kinesis_client):
+    while True: 
+        data = get_data() 
+        print(data) 
+        kinessis_client.put_record(
+            StreamName = stream_name, 
+            Data = json.dumps(data) , 
+            PartitionKey = "partitionkey"
+        )
+
+# dadsadadd
+if __name__ == '__main__':
+    generate(STREAM_NAME, boto3.client("kinesis"))
+
